fd22b9f706d68266474b7174920b6a783dbd9c5f
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-23 11:19:44 -0500

    path

--- src/routers/login.js
+++ src/routers/login.js
@@ -12,5 +12,19 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Error interno del servidor' });
     }
 });
+router.get('/', async (req, res) => {
+    try {
+        res.json({
+            status:200,
+            message:'this is OK'
+        });
+        // const pool = await getPool(req.body); // Obtener el pool antes de ejecutar la consulta
+        // const result = await pool.query('SELECT * FROM tx_productos');
+        // res.json(result.rows);
+    } catch (error) {
+        console.error('Error al ejecutar la consulta:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
 
 module.exports = router;
