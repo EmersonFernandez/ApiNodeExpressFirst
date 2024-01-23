9b079b77f559bfaec746fd6829cd4e09dab58470
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-23 12:33:58 -0500

    ruta de producto

--- src/routers/login.js
+++ src/routers/login.js
@@ -2,18 +2,35 @@ const express = require('express');
 const router = express.Router();
 const getPool = require('../connention'); 
 
-let connEstablished;
-
+let conc;
+let message;
 router.get('/', async (req, res) => {
     data = {
         user:'postgres',
         pass:'1234'
     }
+
     try {
-        const pool = await getPool(data);
-        connEstablished = await getPool(data);;
-        const result = await pool.query('SELECT * FROM tx_productos');
-        res.json(result.rows);
+        conc = () => {
+            const pool =  getPool(data);
+            pool.then(data => {
+                if(data.message){
+                    message = data.message;
+                    return data.message;
+                }else{
+                    message = 'Credeciales Correctas';
+                }
+            });
+
+            return pool;
+        };
+        await conc();
+        //const result =  pool.query('SELECT * FROM tx_productos');
+       // res.json(result.rows);
+        res.json({
+            inf:message
+        });
+        
     } catch (error) {
         console.error('Error al ejecutar la consulta:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
@@ -23,4 +40,4 @@ router.get('/', async (req, res) => {
 
 
 
-module.exports = { router, connEstablished };
+module.exports = { router, conc };
