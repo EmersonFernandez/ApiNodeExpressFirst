2dbfd53c3f852ed3b815ebba90c2877e5dc06b7d
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-23 10:33:45 -0500

    Mensaje descriptivo del commit

--- src/connention.js
+++ src/connention.js
@@ -1,21 +1,21 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// async function getPool(dataUser) {
-//     try {
-//         const pool = await new Pool({
-//             user: dataUser.user,
-//             host: process.env.PGHOST || 'viaduct.proxy.rlwy.net',
-//             database: process.env.PGDATABASE || 'railway',
-//             password: dataUser.pass,
-//             port: process.env.PGPORT || 22927,
-//         });
-//         console.log('Conexión exitosa a la base de datos');
-//         return await pool;
-//     } catch (error) {
-//         console.error('Error al conectar a la base de datos:', error);
-//         return null;
-//     }
-// }
+async function getPool(dataUser) {
+    try {
+        const pool = await new Pool({
+            user: dataUser.user,
+            host: process.env.PGHOST || 'viaduct.proxy.rlwy.net',
+            database: process.env.PGDATABASE || 'railway',
+            password: dataUser.pass,
+            port: process.env.PGPORT || 22927,
+        });
+        console.log('Conexión exitosa a la base de datos');
+        return await pool;
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        return null;
+    }
+}
 
-// module.exports = getPool;
+module.exports = getPool;
---
+++ src/routers/index.js
@@ -0,0 +1,26 @@
+
+
+const fs = require('fs');
+const path = require('path');
+
+function cargarRutas(app) {
+    const directorio = './routers'; // Directorio donde se encuentran tus archivos de ruta
+
+    fs.readdir(directorio, (error, archivos) => {
+        if (error) {
+            console.error('Error al leer el directorio:', error);
+            return;
+        }
+
+        archivos.forEach(nombreArchivo => {
+            const rutaArchivo = path.join(directorio, nombreArchivo);
+            const nombreRouter = nombreArchivo.substring(0, nombreArchivo.indexOf('.'));
+            if (nombreRouter !== 'index' && nombreArchivo.endsWith('.js')) {
+                const router = require(`../${rutaArchivo}`);
+                app.use(`/api/${nombreRouter}`, router);
+            }
+        });
+    });
+}
+
+module.exports = cargarRutas;
