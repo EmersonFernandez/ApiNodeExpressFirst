65db08ab1b4e5ec36a8d07358496468d74d1d329
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-23 10:46:13 -0500

    path

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 function cargarRutas(app) {
-    const directorio = './routers';
+    const directorio = 'C:/Users/ferna/OneDrive/Escritorio/CurosApis/aPgi/src/routers';
     console.log("este es el Dirrectorio: >>>>>  " ,directorio);
     fs.readdir(directorio, (error, archivos) => {
         if (error) {
