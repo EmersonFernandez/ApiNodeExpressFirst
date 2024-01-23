0cbb45245a0f48f5465b84f8b053eb203cf66691
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-23 18:26:24 -0500

    ruta de producto

--- src/routers/login.js
+++ src/routers/login.js
@@ -4,8 +4,8 @@ const getPool = require('../connention');
 
 router.get('/', async (req, res) => {
     const { user, pass } = req.body;
-    process.env.PASS =  'emerson';
-    process.env.USER =  '1234';
+    process.env.PASS =  '1234';
+    process.env.USER =  'emerson';
     // process.env.PASS =  pass;
     // process.env.USER =  user;
 
