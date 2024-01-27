a1e057e9ffc37a1077e8acf624b67c7ef4e6cb17
EmersonFernandez <fernandezreginoe@gmail.com>
2024-01-27 15:22:11 -0500

    ruta de producto

--- src/routers/producto.js
+++ src/routers/producto.js
@@ -1,52 +1,174 @@
 const express = require('express');
 const router = express.Router();
 const getPool = require('../connention');
-const { closeConnection,validarToken } = require('../fuctiones');
-// require('dotenv').config();
-// require('../connention');
-// const {connection} = require('./login');
+const { closeConnection, validarToken } = require('../fuctiones');
 
 
-router.get('/', validarToken ,async (req, res) => {
+
+router.get('/', validarToken, async (req, res) => {
     try {
-
         const token = req.cookies.token;
-
         if (!token) {
             return res.json(
                 {
-                    error:true,
-                    errorMessage: 'No hay token, acceso no autorizado' }
-
-                );
+                    error: true,
+                    errorMessage: 'No hay token, acceso no autorizado'
+                }
+            );
         }
-
         const pool = await getPool();
         const result = await pool.query('SELECT * FROM tx_productos');
-
-
-
         res.json({
             status: 200,
             error: false,
             des: 'ruta de productos',
             message: 'this is OK',
-            token:req.results,
-            results : result.rows
+            token: req.results,
+            results: result.rows
         });
-
-        closeConnection(pool,res);
-
+        closeConnection(pool, res);
     } catch (error) {
         console.error('Error al ejecutar la consulta:', error);
-        res.json({ 
-            status:500,
-            error:true,
-            errorDes: 'Error interno del servidor', 
-            errorMessage: error.message 
+        res.json({
+            status: 500,
+            error: true,
+            errorDes: 'Error interno del servidor',
+            errorMessage: error.message
+        });
+    }
+});
+
+router.post('/', validarToken, async (req, res) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) {
+            return res.json({
+                error: true,
+                errorMessage: 'No hay token, acceso no autorizado'
+            });
+        }
+
+        const { codProducto, nombre, descripcion, precio } = req.body;
+
+        const pool = await getPool();
+
+        const resultSeq = await pool.query('SELECT max(NCODIGO) + 1 as seq FROM tx_productos');
+        const seq = resultSeq.rows[0].seq;  
+
+        const query = 'INSERT INTO tx_productos (NCODIGO, NCODIGO_PRODUCTO, VNOMBRE, VDESCRIPCION, NPRECIO) VALUES ($1, $2, $3, $4, $5)';
+
+        const result = await pool.query(query, [seq, codProducto, nombre, descripcion, precio]);
+
+        if (result.rowCount > 0) {
+            res.json({
+                status: 200,
+                error: false,
+                message: 'Se guardó correctamente',
+                results: result.rows[0]
+            });
+        } else {
+            res.json({
+                status: 500,
+                error: true,
+                message: 'Error al guardar los campos',
+                results: null
+            });
+        }
+
+        closeConnection(pool, res);
+
+    } catch (error) {
+        console.error('Error al ejecutar el insert:', error);
+        res.json({
+            status: 500,
+            error: true,
+            errorDes: 'Error interno del servidor',
+            errorMessage: error.message
         });
     }
 });
 
 
-module.exports = {router};
+
+router.put('/', async (req,res) => {
+    try{
+        const token = req.cookies.token;
+        if(!token){
+            return res.json({
+                error: true,
+                errorMessage: 'No hay token, acceso no autorizado'
+            });
+        }
+        const { codigo ,codProducto, nombre, descripcion, precio } = req.body;
+        const pool = await getPool();
+        const query = 'UPDATE tx_productos SET NCODIGO_PRODUCTO = $2, VNOMBRE = $3, VDESCRIPCION = $4, NPRECIO = $5 WHERE NCODIGO = $1';
+        const result = await pool.query(query,[codigo,codProducto,nombre,descripcion,precio]);
+
+        if (result.rowCount > 0) {
+            res.json({
+                status: 200,
+                error: false,
+                message: 'Se actualizo correctamente',
+                results: result.rows[0]
+            });
+        } else {
+            res.json({
+                status: 500,
+                error: true,
+                message: 'Error al actualizar los campos',
+                results: null
+            });
+        }
+
+        closeConnection(pool, res);
+
+    }catch(error){
+        console.error('Error al ejecutar el update:', error);
+        res.json({
+            status: 500,
+            error: true,
+            errorDes: 'Error interno del servidor',
+            errorMessage: error.message
+        });
+    }
+});
+
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const id = req.params.id;
+        const pool = await getPool();
+        const result = await pool.query('DELETE FROM tx_productos WHERE NCODIGO = $1',[id]);
+
+        if (result.rowCount > 0) {
+
+            res.json({
+                status: 200,
+                error: false,
+                message: 'Producto eliminado correctamente',
+                results: result.rows[0]
+            });
+        } else {
+            res.json({
+                status: 404,
+                error: true,
+                message: 'No se encontró el producto con el ID proporcionado',
+                result:null
+            });
+        }
+
+        closeConnection(pool, res);
+        
+    } catch (error) {
+        console.error('Error al ejecutar el delete:', error);
+        res.json({
+            status: 500,
+            error: true,
+            errorDes: 'Error interno del servidor',
+            errorMessage: error.message
+        }); 
+    }
+});
+
+module.exports = { router };
