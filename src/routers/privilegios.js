5da514791866eb2bf6fc6119fd74dd90518e58f9
EmersonFernandez <fernandezreginoe@gmail.com>
2024-03-18 17:01:20 -0500

    ruta de usuarios

--- src/routers/privilegios.js
+++ src/routers/privilegios.js
@@ -3,16 +3,48 @@ const router = express.Router();
 
 const getPool = require('../connection');
 const { closeConnection } = require('../funciones');
+const {validarToken} = require('../funciones');
 
 
 // rutas
-router.get('/', (req, res) => {
-    
+router.get('/', validarToken , async (req, res) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.json(
+                {
+                    error: true,
+                    errorMessage: 'No hay token, acceso no autorizado'
+                }
+            );
+        }
+
+        const pool = await getPool();
+        const result = await pool.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`);
+            res.json({
+                status: 200,
+                error: false,
+                des: 'ruta de productos',
+                message: 'this is OK',
+                token: req.results,
+                results: result.rows
+            });
+
+        closeConnection(pool, res);
+    } catch (error) {
+        console.error('Error al ejecutar la consulta:', error);
+        return res.json({
+            status: 500,
+            error: true,
+            errorDes: 'Error interno del servidor',
+            erroMesagge: error.message
+        });
+    }
 });
 
 // establecer los roles a las tablas
 
-router.post('/',async (req,res) => {
+router.post('/',validarToken,async (req,res) => {
     try {
         const token = req.cookies.token;
         if (!token) {
