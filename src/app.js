c86f233269da18a9d13e885cd25021926835a6e0
EmersonFernandez <fernandezreginoe@gmail.com>
2024-03-31 16:08:10 -0500

    ruta de usuarios

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ let port = 3000 || process.env.PORT;
 
 // cors opciones
 const corsOptions = {
-    origin: 'http://127.0.0.7:5500',
+    origin: 'http://localhost:5173/',
     credentials: true,
 };
 
